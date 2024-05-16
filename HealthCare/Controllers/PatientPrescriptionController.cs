6495056c4f9988c830fc1f86b3b208bda35fd098
admin <itmanager@stellartechpdkt.org>
2024-05-16 18:01:54 +0530

    Rajalingam-Added PatientPrescription and prescription print

--- HealthCare/Controllers/PatientPrescriptionController.cs
+++ HealthCare/Controllers/PatientPrescriptionController.cs
@@ -1,9 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using HealthCare.Business;
+using HealthCare.Context;
+using HealthCare.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HealthCare.Controllers
 {
     public class PatientPrescriptionController : Controller
     {
+
+        private HealthcareContext GetPrescription;
+
+        public PatientPrescriptionController(HealthcareContext GetPrescription)
+        {
+            this.GetPrescription = GetPrescription;
+        }
+
+
+        public async Task<IActionResult> Prescription(PatientEPrescriptionModel pPres)
+        {
+
+            var existingCat = await GetPrescription.SHprsPrescription.FindAsync(pPres.PatientID,pPres.OrderID,pPres.CaseVisitID,pPres.DrugID);
+            if (existingCat != null)
+            {
+
+                existingCat.PatientID = pPres.PatientID;
+                existingCat.EpressID = pPres.EpressID;
+                existingCat.CaseVisitID = pPres.CaseVisitID;
+                existingCat.OrderID=pPres.OrderID;
+                existingCat.DrugID=pPres.DrugID;
+                existingCat.DoctorID=pPres.DoctorID;
+                existingCat.PrescriptionDate=pPres.PrescriptionDate;
+                existingCat.Unit=pPres.Unit;
+                existingCat.UnitCategory=pPres.UnitCategory;
+                existingCat.Frequency=pPres.Frequency;
+                existingCat.FrequencyUnit=pPres.FrequencyUnit;
+                existingCat.Duration=pPres.Duration;
+                existingCat.Quantity=pPres.Quantity;
+                existingCat.EndDate=pPres.EndDate;
+                existingCat.RouteAdmin=pPres.RouteAdmin;
+                existingCat.Instructions=pPres.Instructions;
+                existingCat.Comments=pPres.Comments;
+                existingCat.FillDate=pPres.FillDate;
+                existingCat.Result=pPres.Result;
+                existingCat.lastupdatedDate = DateTime.Now.ToString();
+                existingCat.lastUpdatedUser = "Myself";
+                existingCat.lastUpdatedMachine = "Lap";
+            }
+            else
+            {
+                pPres.lastupdatedDate = DateTime.Now.ToString();
+                pPres.lastUpdatedUser = "Myself";
+                pPres.lastUpdatedMachine = "Lap";
+                GetPrescription.SHprsPrescription.Add(pPres);
+
+            }
+            await GetPrescription.SaveChangesAsync();
+            ViewBag.Message = "Saved Successfully.";
+            return View("PatientEPrescription", pPres);
+
+
+        }
+
+        public async Task<IActionResult> PrescriptionPrint (PatientEPrescriptionPrintModel Model)
+        {
+            BusinessClassExamination ObjPrintResult = new BusinessClassExamination(GetPrescription);
+
+            if (ObjPrintResult != null)
+            {
+                var prescription = await ObjPrintResult.GetPrescriptionPrint(Model.PatientID, Model.OrderID,Model.CaseVisitID);
+                return View("PatientEPrescriptionPrint", prescription);
+            }
+            return View(Model);
+        }
+
+
+
+
+
         public IActionResult Index()
         {
             return View();
--- HealthCare/Models/DrugRackModel.cs
+++ HealthCare/Models/DrugRackModel.cs
@@ -14,10 +14,10 @@
 
         public string RackID { get => rackID; set => rackID = value; }
         public string RackName { get => rackName; set => rackName = value; }
-        public string UniqueIdentifier { get => uniqueIdentifier; set => uniqueIdentifier = value; }
-        public string Comments { get => comments; set => comments = value; }
-        public string lastUpdatedDate { get => strLastUpdatedDate; set => strLastUpdatedDate = value; }
-        public string lastUpdatedUser { get => strLastUpdatedUser; set => strLastUpdatedUser = value; }
-        public string lastUpdatedmachine { get => strLastUpdatedmachine; set => strLastUpdatedmachine = value; }
+        public string? UniqueIdentifier { get => uniqueIdentifier; set => uniqueIdentifier = value; }
+        public string? Comments { get => comments; set => comments = value; }
+        public string? lastUpdatedDate { get => strLastUpdatedDate; set => strLastUpdatedDate = value; }
+        public string? lastUpdatedUser { get => strLastUpdatedUser; set => strLastUpdatedUser = value; }
+        public string? lastUpdatedmachine { get => strLastUpdatedmachine; set => strLastUpdatedmachine = value; }
     }
 }
