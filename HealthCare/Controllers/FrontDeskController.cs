2e2fee6d0e3a1020ef525f8073fac4ebc9971640
StelllarAdmin <itmanager@stellartechpdkt.org>
2024-05-16 15:39:27 +0530

    Manimegalai: FrontDesk menu and sub menu added

---
+++ HealthCare/Controllers/FrontDeskController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCare.Controllers
+{
+    public class FrontDeskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OpChecking()
+        {
+            return View();
+        }
+        public IActionResult IpRegistration()
+        {
+            return View();
+        }
+        public IActionResult OpDischarge()
+        {
+            return View();
+        }
+        
+    }
+}
