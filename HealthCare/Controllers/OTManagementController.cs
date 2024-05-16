b09f4247602dd4c9255195a4de59baf6dbc4cbf3
admin <itmanager@stellartechpdkt.org>
2024-05-16 20:24:50 +0530

    purusoth: add OT Managment menu

---
+++ HealthCare/Controllers/OTManagementController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCare.Controllers
+{
+    public class OTManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OTScheduling()
+        {
+            return View();
+        }
+        public IActionResult OTConformation()
+        {
+            return View();
+        }
+        public IActionResult OTNotes()
+        {
+            return View();
+        }
+        public IActionResult OTSummary()
+        {
+            return View();
+        }
+    }
+}
--- HealthCare/Migrations/20240516142854_Rmigmodeladd.Designer.cs
+++ HealthCare/Migrations/20240516142854_Rmigmodeladd.Designer.cs
@@ -4,6 +4,7 @@ using HealthCare.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HealthCare.Migrations
 {
     [DbContext(typeof(HealthcareContext))]
-    partial class HealthcareContextModelSnapshot : ModelSnapshot
+    [Migration("20240516142854_Rmigmodeladd")]
+    partial class Rmigmodeladd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
