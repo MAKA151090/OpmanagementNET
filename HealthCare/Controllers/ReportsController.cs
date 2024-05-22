ece1443504b4c8e73b519c335b696adca97472e0
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-05-22 18:47:10 +0530

    KK: Added migration for doc schedule module

---
+++ HealthCare/Controllers/ReportsController.cs
@@ -0,0 +1,43 @@
+﻿using HealthCare.Business;
+using HealthCare.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Data;
+using System.Dynamic;
+using System.Globalization;
+
+namespace HealthCare.Controllers
+{
+    [Authorize]
+    public class ReportsController : Controller
+    {
+               
+        private HealthcareContext _healthcareContext;
+        public ReportsController(HealthcareContext healthcareContext)
+        {
+            _healthcareContext = healthcareContext;
+
+        }
+        public IActionResult Reports()
+        {
+
+            var query = BusinessClassCommon.DataTable(_healthcareContext, "Select * from SHTestMaster");
+
+
+            return View("Reports", query);
+        }
+        [HttpPost]
+        public IActionResult GetReports()
+        {
+
+
+            var query = BusinessClassCommon.DataTable(_healthcareContext, "Select * from SHTestMaster");
+               
+
+            return View("Reports",query);
+        }
+
+
+    }
+}
