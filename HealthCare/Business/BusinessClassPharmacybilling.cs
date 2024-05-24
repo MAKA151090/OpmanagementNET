cb0f7a361e45542f31c90d0b7f42c19f8a4a9b24
admin <itmanager@stellartechpdkt.org>
2024-05-24 10:20:54 +0530

    Mk - added business class for pharmacy biling. added resource type

---
+++ HealthCare/Business/BusinessClassPharmacybilling.cs
@@ -0,0 +1,17 @@
+﻿using HealthCare.Context;
+using HealthCare.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCare.Business
+{
+    public class BusinessClassPharmacybilling : Controller
+    {
+        private readonly HealthcareContext _healthcareContext;
+
+        public BusinessClassPharmacybilling(HealthcareContext healthcareContext)
+        {
+            _healthcareContext = healthcareContext;
+        }
+
+    }
+}
