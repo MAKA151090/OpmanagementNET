5277ac469624cdf3927035cafcc1e4ad46e09c6a
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-05-30 13:57:52 +0530

    KK: Migration 37

---
+++ HealthCare/Migrations/20240530082647_Intial37.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial37 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SHExmSeverity",
+                table: "SHExmSeverity");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Symptoms",
+                table: "SHExmSeverity",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Severity",
+                table: "SHExmSeverity",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SHExmSeverity",
+                table: "SHExmSeverity",
+                columns: new[] { "PatientID", "FacilityID", "VisitID", "ExaminationID", "Symptoms" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SHExmSeverity",
+                table: "SHExmSeverity");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Severity",
+                table: "SHExmSeverity",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Symptoms",
+                table: "SHExmSeverity",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SHExmSeverity",
+                table: "SHExmSeverity",
+                columns: new[] { "PatientID", "FacilityID", "VisitID", "ExaminationID", "Severity" });
+        }
+    }
+}
