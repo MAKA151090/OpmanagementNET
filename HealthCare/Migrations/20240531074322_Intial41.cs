ecb42a9226358dcbdf7d91f993936bbdb76e4622
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-05-31 13:14:19 +0530

    KK: Migration 41 is delete and active column changes

---
+++ HealthCare/Migrations/20240531074322_Intial41.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial41 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Active",
+                table: "SHclnResourceSchedule",
+                newName: "StrInActive");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "StrIsDelete",
+                table: "SHclnViewResourceSchedule",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StrIsDelete",
+                table: "SHclnViewResourceSchedule");
+
+            migrationBuilder.RenameColumn(
+                name: "StrInActive",
+                table: "SHclnResourceSchedule",
+                newName: "Active");
+        }
+    }
+}
