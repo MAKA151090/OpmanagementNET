39a6b2281ef5b2f0225ef0de16882e79642e6b64
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-06-04 22:21:41 +0530

    KK: Migration 43

---
+++ HealthCare/Migrations/20240604165046_Intial43.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial43 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "SHInpatientAdmission",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "SHInpatientAdmission");
+        }
+    }
+}
