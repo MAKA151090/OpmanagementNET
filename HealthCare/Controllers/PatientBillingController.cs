d163081c7be0b556b7705623337c9b32482d29a4
admin <itmanager@stellartechpdkt.org>
2024-06-10 09:38:43 +0530

    Muthukumar - adding new patient payment billing

--- HealthCare/Controllers/PatientBillingController.cs
+++ HealthCare/Controllers/PatientBillingController.cs
@@ -1,7 +1,9 @@
 ﻿using HealthCare.Business;
 using HealthCare.Context;
+using HealthCare.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HealthCare.Controllers
 {
@@ -40,6 +42,38 @@ namespace HealthCare.Controllers
                 }
             
         }
+
+        public async Task<IActionResult> PatientPayment(PatientPaymentModel model)
+        {
+            var existingpayment = await _healthcareContext.SHPatientPayment.FindAsync(model.PaymentID);
+            if (existingpayment != null)
+            {
+               existingpayment.PatientID = model.PaymentID;
+                existingpayment.CaseVisitID = model.CaseVisitID;
+                existingpayment.BillID = model.BillID;
+                existingpayment.PaymentID = model.PaymentID;
+                existingpayment.lastUpdatedDate = DateTime.Now.ToString();
+                existingpayment.lastUpdatedUser = User.Claims.First().Value.ToString();
+                existingpayment.lastUpdatedMachine = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+
+                    _healthcareContext.Entry(existingpayment).State = EntityState.Modified;
+            }
+            else
+            {
+
+                model.lastUpdatedDate = DateTime.Now.ToString();
+                model.lastUpdatedUser = User.Claims.First().Value.ToString();
+                model.lastUpdatedMachine = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+                _healthcareContext.SHPatientPayment.Add(model);
+
+            }
+
+           await _healthcareContext.SaveChangesAsync();
+
+            ViewBag.Message = "Saved Successfully.";
+            return View(model);
+        }
+        
 
         public IActionResult Index()
         {
---
+++ HealthCare/Models/PatientPaymentBillDetailsModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthCare.Models
+{
+    public class PatientPaymentBillDetailsModel
+    {
+        public PatientPaymentBillDetailsModel() { }
+
+
+        private string strPaymentID;
+        private string strPaymentDetailID;
+        private string strPatientID;
+        private string strPaymentDescription;
+        private string strDateandTime;
+        private string strAmountPaid;
+        private string strPaymentMode;
+        private string strTransactionDetails;
+        private string strOtherComments;
+        private bool strIsDelte;
+        private string strPaymentDate;
+        private string strlastUpdatedDate;
+        private string strlastUpdatedUser;
+        private string strlastUpdatedMachine;
+
+        public string PaymentID { get => strPaymentID; set => strPaymentID = value; }
+
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string PaymentDetailID { get => strPaymentDetailID; set => strPaymentDetailID = value; }
+        public string? PatientID { get => strPatientID; set => strPatientID = value; }
+        public string? PaymentDescription { get => strPaymentDescription; set => strPaymentDescription = value; }
+        public string? DateandTime { get => strDateandTime; set => strDateandTime = value; }
+        public string? AmountPaid { get => strAmountPaid; set => strAmountPaid = value; }
+        public string? PaymentMode { get => strPaymentMode; set => strPaymentMode = value; }
+        public string? TransactionDetails { get => strTransactionDetails; set => strTransactionDetails = value; }
+        public string? OtherComments { get => strOtherComments; set => strOtherComments = value; }
+        public bool IdDelte { get => strIsDelte; set => strIsDelte = value; }
+        public string? PaymentDate { get => strPaymentDate; set => strPaymentDate = value; }
+        public string? lastUpdatedDate { get => strlastUpdatedDate; set => strlastUpdatedDate = value; }
+        public string? lastUpdatedUser { get => strlastUpdatedUser; set => strlastUpdatedUser = value; }
+        public string? lastUpdatedMachine { get => strlastUpdatedMachine; set => strlastUpdatedMachine = value; }
+    }
+}
