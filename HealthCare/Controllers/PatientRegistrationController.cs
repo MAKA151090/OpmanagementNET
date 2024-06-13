dcb59f5dce3b617c70d680297fba662d3ce8296e
admin <itmanager@stellartechpdkt.org>
2024-06-13 18:59:23 +0530

    Muthukumar chanegs in patient registration and payments

--- HealthCare/Controllers/PatientRegistrationController.cs
+++ HealthCare/Controllers/PatientRegistrationController.cs
@@ -23,76 +23,70 @@ namespace HealthCare.Controllers
 
         }
         [HttpPost]
-        public async Task<IActionResult> Create(PatientRegistrationModel model)
+        public async Task<IActionResult> CreateOrUpdate(PatientRegistrationModel model, string patientID, string facilityID)
         {
-         
-            var exitingpatient = await _healthcareContext.SHPatientRegistration.FindAsync(model.PatientID);
-            if (exitingpatient != null)
+            // Check if the patientID and facilityID are provided
+            if (!string.IsNullOrEmpty(patientID) && !string.IsNullOrEmpty(facilityID))
             {
-                exitingpatient.PatientID = model.PatientID;
-                exitingpatient.FacilityID = model.FacilityID;
-                exitingpatient.FirstName = model.FirstName;
-                exitingpatient.MidName = model.MidName;
-                exitingpatient.LastName = model.LastName;
-                exitingpatient.FullName = model.FullName;
-                exitingpatient.Initial = model.Initial;
-                exitingpatient.Prefix = model.Prefix;
-                exitingpatient.Dob = model.Dob;
-                exitingpatient.Age = model.Age;
-                exitingpatient.Gender = model.Gender;
-                exitingpatient.BloodGroup = model.BloodGroup;
-                exitingpatient.PhoneNumber = model.PhoneNumber;
-                exitingpatient.MaritalStatus = model.MaritalStatus;
-                exitingpatient.Address1 = model.Address1;
-                exitingpatient.Address2 = model.Address2;
-                exitingpatient.Country = model.Country;
-                exitingpatient.City = model.City;
-                exitingpatient.State = model.State;
-                exitingpatient.Pin = model.Pin;
-                exitingpatient.IDPrfName = model.IDPrfName;
-                exitingpatient.IDPrfNumber = model.IDPrfNumber;
-                exitingpatient.CnctPrsnName = model.CnctPrsnName;
-                exitingpatient.Rlnpatient = model.Rlnpatient;
-                exitingpatient.EmgcyCntNum = model.EmgcyCntNum;
-                exitingpatient.lastUpdatedDate = DateTime.Now.ToString();
-                exitingpatient.lastUpdatedUser = User.Claims.First().Value.ToString();
-                _healthcareContext.Entry(exitingpatient).State = EntityState.Modified;
+                // Retrieve existing patient data if available
+                var existingPatient = await _healthcareContext.SHPatientRegistration.FindAsync(patientID);
+                if (existingPatient != null)
+                {
+                    // Update existing patient data
+                    existingPatient.FacilityID = model.FacilityID;
+                    existingPatient.FirstName = model.FirstName;
+                    existingPatient.MidName = model.MidName;
+                    existingPatient.LastName = model.LastName;
+                    existingPatient.FullName = model.FullName;
+                    existingPatient.Initial = model.Initial;
+                    existingPatient.Prefix = model.Prefix;
+                    existingPatient.Dob = model.Dob;
+                    existingPatient.Age = model.Age;
+                    existingPatient.Gender = model.Gender;
+                    existingPatient.BloodGroup = model.BloodGroup;
+                    existingPatient.PhoneNumber = model.PhoneNumber;
+                    existingPatient.MaritalStatus = model.MaritalStatus;
+                    existingPatient.Address1 = model.Address1;
+                    existingPatient.Address2 = model.Address2;
+                    existingPatient.Country = model.Country;
+                    existingPatient.City = model.City;
+                    existingPatient.State = model.State;
+                    existingPatient.Pin = model.Pin;
+                    existingPatient.IDPrfName = model.IDPrfName;
+                    existingPatient.IDPrfNumber = model.IDPrfNumber;
+                    existingPatient.CnctPrsnName = model.CnctPrsnName;
+                    existingPatient.Rlnpatient = model.Rlnpatient;
+                    existingPatient.EmgcyCntNum = model.EmgcyCntNum;
+                    existingPatient.lastUpdatedDate = DateTime.Now.ToString();
+                    existingPatient.lastUpdatedUser = User.Claims.First().Value.ToString();
+                    _healthcareContext.Entry(existingPatient).State = EntityState.Modified;
+                }
+                else
+                {
+                    // Create new patient data
+                    model.PatientID = patientID;
+                    model.FacilityID = facilityID;
+                    model.lastUpdatedDate = DateTime.Now.ToString();
+                    model.lastUpdatedUser = User.Claims.First().Value.ToString();
+                    _healthcareContext.SHPatientRegistration.Add(model);
+                }
 
-
-            }
-            else
-            {
-
-                model.lastUpdatedDate = DateTime.Now.ToString();
-                model.lastUpdatedUser = User.Claims.First().Value.ToString();
-                _healthcareContext.SHPatientRegistration.Add(model);
-            }
                 await _healthcareContext.SaveChangesAsync();
-            ViewBag.Message = "Saved Successfully";
+                ViewBag.Message = "Saved Successfully";
+            }
 
-              BusinessClassRegistration schedule = new BusinessClassRegistration(_healthcareContext);
+            // Fetch patient data for the provided patientID and facilityID
+            var schedule = new BusinessClassRegistration(_healthcareContext);
+            var patient = await schedule.GetPatientObjectiveSubmit(patientID, facilityID);
+
+            // Pass necessary view data
             ViewData["bldgrpid"] = schedule.GetBloodGroup();
             ViewData["Facid"] = schedule.GetFacilityid();
 
-            return View("PatientRegister" , model);           
-
+            // Return the view with the patient data
+            return View("PatientRegister", patient ?? new PatientRegistrationModel());
         }
 
-
-        public async Task<IActionResult> GetPatient(string patientID, string facilityID)
-        {
-
-            BusinessClassRegistration schedule = new BusinessClassRegistration(_healthcareContext);
-               var patient = await schedule.GetPatientObjectiveSubmit(patientID, facilityID);
-            if (patient != null)
-            {
-                ViewData["bldgrpid"] = schedule.GetBloodGroup();
-                ViewData["Facid"] = schedule.GetFacilityid();
-                return View("PatientRegister", patient);
-            }
-
-            return View("PatientRegister");
-        }
 
         [HttpPost]
         public async Task<IActionResult> PatientScheduling(DoctorScheduleModel model, string action, List<string> selectedSchedules,string staffID,string FacilityID,string Date,String patientID)
