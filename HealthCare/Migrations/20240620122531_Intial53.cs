d16ceb2ce905e76fa0d9fba4401cf20ecc412e81
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-06-20 17:56:51 +0530

    rajalingam-migration

---
+++ HealthCare/Migrations/20240620122531_Intial53.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial53 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TaxID",
+                table: "SHpayrollTax",
+                newName: "Taxtype");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Primary",
+                table: "SHBankdetails",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Primary",
+                table: "SHBankdetails");
+
+            migrationBuilder.RenameColumn(
+                name: "Taxtype",
+                table: "SHpayrollTax",
+                newName: "TaxID");
+        }
+    }
+}
