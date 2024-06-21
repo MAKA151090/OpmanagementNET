cd8ebd296516c3627ea80d0d5b9298320a127c1b
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-06-21 15:19:50 +0530

    Rajalingam-Added Migration

---
+++ HealthCare/Migrations/20240621094838_Intial55.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial55 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SHpayrollTax",
+                table: "SHpayrollTax");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Taxtype",
+                table: "SHpayrollTax",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxSlotID",
+                table: "SHpayrollTax",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SHpayrollTax",
+                table: "SHpayrollTax",
+                columns: new[] { "PayrollID", "TaxSlotID", "StaffID" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SHpayrollTax",
+                table: "SHpayrollTax");
+
+            migrationBuilder.DropColumn(
+                name: "TaxSlotID",
+                table: "SHpayrollTax");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Taxtype",
+                table: "SHpayrollTax",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SHpayrollTax",
+                table: "SHpayrollTax",
+                columns: new[] { "PayrollID", "Taxtype", "StaffID" });
+        }
+    }
+}
