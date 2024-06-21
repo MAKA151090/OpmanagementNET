df00ed781b2b8847cedf717953a528415e27296c
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-06-21 16:00:56 +0530

    Rajalingam-mig

---
+++ HealthCare/Migrations/20240621102952_Intial56.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial56 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDelete",
+                table: "SHpayroll",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsDelete",
+                table: "SHpayroll",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
