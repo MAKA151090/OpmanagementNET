f94e293796cef3ac1939e91f208ae4d36ab68c40
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-06-24 19:27:14 +0530

    Rajalingam-migration

---
+++ HealthCare/Migrations/20240624135406_Intial58.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial58 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShclnrollAccessmaster",
+                columns: table => new
+                {
+                    StaffID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RollName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LastupdatedDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastupdatedMachine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lastupdateduser = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShclnrollAccessmaster", x => new { x.StaffID, x.RollName });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shclnrolltypemaster",
+                columns: table => new
+                {
+                    RollID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LastupdatedUser = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastupdatedDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastupdatedMachine = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RollName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shclnrolltypemaster", x => x.RollID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShclnrollAccessmaster");
+
+            migrationBuilder.DropTable(
+                name: "Shclnrolltypemaster");
+        }
+    }
+}
