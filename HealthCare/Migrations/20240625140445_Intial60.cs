d65bdf47165ff29c2a698dd13748ce702a5c3c43
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-06-25 19:35:44 +0530

    Rajalingam-add mig

---
+++ HealthCare/Migrations/20240625140445_Intial60.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial60 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RollName",
+                table: "ShclnrollAccessmaster",
+                newName: "RollID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RollID",
+                table: "ShclnrollAccessmaster",
+                newName: "RollName");
+        }
+    }
+}
