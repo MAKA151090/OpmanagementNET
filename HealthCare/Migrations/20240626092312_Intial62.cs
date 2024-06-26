2fb6ed0f04038ac9d11b2d48af99ba9d5021260e
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-06-26 14:54:22 +0530

    Rajalingam-Added mig

---
+++ HealthCare/Migrations/20240626092312_Intial62.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial62 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "shdbscreenname",
+                columns: table => new
+                {
+                    ScreenName = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_shdbscreenname", x => x.ScreenName);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "shdbscreenname");
+        }
+    }
+}
