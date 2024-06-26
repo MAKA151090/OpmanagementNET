5baf75a7d47e7f87b55cca06fdcb094a97c263a1
makaevent1510@gmail.com <makaevent1510@gmail.com>
2024-06-26 12:01:25 +0530

    Rajalingam-Added migration

---
+++ HealthCare/Migrations/20240626062615_Intial61.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial61 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SHClnRollAccess",
+                table: "SHClnRollAccess");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastUpdatedUser",
+                table: "SHClnRollAccess",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastUpdatedMachine",
+                table: "SHClnRollAccess",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastUpdatedDate",
+                table: "SHClnRollAccess",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ScreenID",
+                table: "SHClnRollAccess",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SHClnRollAccess",
+                table: "SHClnRollAccess",
+                columns: new[] { "RollID", "ScreenID" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SHClnRollAccess",
+                table: "SHClnRollAccess");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastUpdatedUser",
+                table: "SHClnRollAccess",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastUpdatedMachine",
+                table: "SHClnRollAccess",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastUpdatedDate",
+                table: "SHClnRollAccess",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ScreenID",
+                table: "SHClnRollAccess",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SHClnRollAccess",
+                table: "SHClnRollAccess",
+                column: "RollID");
+        }
+    }
+}
